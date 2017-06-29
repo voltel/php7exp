440ed2e7893405436038083d556d5883a38cb75a
Volodymyr Telnov <volodymyr.telnov@gmail.com>
2017-06-29 19:05:57 +0300

    Update db_credentials.php

--- app/config/db_credentials.php
+++ app/config/db_credentials.php
@@ -5,7 +5,7 @@ return $a_db_credentials = array(
   'driver' => 'pdo_mysql',
   'host' => '127.0.0.1:3306',
   'dbname' => 'mydb',
-  'user' => 'volodymyr',
-  'password' => 'pathword',
+  'user' => '',
+  'password' => '',
   'charset' => 'utf8mb4'
 );
